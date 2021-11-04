7ff839bdac73c54969d6782571746b3393f4f389
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-04 19:29:08 +0700

    Create NewThread.test.js

---
+++ src/Domains/threads/_test/NewThread.test.js
@@ -0,0 +1,43 @@
+const NewThread = require('../NewThread');
+
+describe('a NewThread entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const userId = 'user-xxx';
+    const payload = {
+      title: 'abc',
+    };
+
+    // Action and Assert
+    expect(() => new NewThread(userId, payload)).toThrowError('NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const userId = 'user-xxx';
+    const payload = {
+      title: 123,
+      body: true,
+    };
+
+    // Action and Assert
+    expect(() => new NewThread(userId, payload)).toThrowError('NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create newThread object correctly', () => {
+    // Arrange
+    const expectedUserId = 'user-xxx';
+    const payload = {
+      title: 'abc',
+      body: 'def',
+    };
+
+    // Action
+    const { userId, title, body } = new NewThread(expectedUserId, payload);
+
+    // Assert
+    expect(userId).toEqual(expectedUserId);
+    expect(title).toEqual(payload.title);
+    expect(body).toEqual(payload.body);
+  });
+});
