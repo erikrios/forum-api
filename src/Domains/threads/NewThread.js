18fd44d4eb846d97faab00e53af9bcbebd58c3ec
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-04 19:29:18 +0700

    Create NewThread.js

---
+++ src/Domains/threads/NewThread.js
@@ -0,0 +1,23 @@
+class NewThread {
+  constructor(userId, payload) {
+    this._verifyPayload(payload);
+
+    const { title, body } = payload;
+
+    this.userId = userId;
+    this.title = title;
+    this.body = body;
+  }
+
+  _verifyPayload({ title, body }) {
+    if (!title || !body) {
+      throw new Error('NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof title !== 'string' || typeof body !== 'string') {
+      throw new Error('NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = NewThread;
