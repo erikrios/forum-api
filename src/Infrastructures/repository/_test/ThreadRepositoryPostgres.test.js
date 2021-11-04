2b490019dff35504a648e49a932bc4487f3e46a7
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-04 22:31:01 +0700

    Create ThreadRepositoryPostgres.test.js

---
+++ src/Infrastructures/repository/_test/ThreadRepositoryPostgres.test.js
@@ -0,0 +1,78 @@
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const NewThread = require('../../../Domains/threads/entities/NewThread');
+const AddedThread = require('../../../Domains/threads/entities/AddedThread');
+const pool = require('../../database/postgres/pool');
+const ThreadRepositoryPostgres = require('../ThreadRepositoryPostgres');
+
+describe('ThreadRepositoryPostgres', () => {
+  afterEach(async () => {
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('addThread function', () => {
+    it('should persist new thread and return added thread correctly', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+        password: 'password',
+        fullname: 'dicoding indonesia',
+        username: 'dicoding',
+      });
+
+      const newThread = new NewThread(
+        'user-123',
+        {
+          title: 'thread title',
+          body: 'thread body',
+        },
+      );
+
+      const fakeIdGenerator = () => '123';
+      const threadRepositoryPostgres = new ThreadRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      await threadRepositoryPostgres.addThread(newThread);
+
+      // Assert
+      const threads = await ThreadsTableTestHelper.findThreadById('thread-123');
+      expect(threads).toHaveLength(1);
+    });
+
+    it('should return added thread correctly', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+        password: 'password',
+        fullname: 'dicoding indonesia',
+        username: 'dicoding',
+      });
+
+      const newThread = new NewThread(
+        'user-123',
+        {
+          title: 'thread title',
+          body: 'thread body',
+        },
+      );
+
+      const fakeIdGenerator = () => '123';
+      const threadRepositoryPostgres = new ThreadRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      const addedThread = await threadRepositoryPostgres.addThread(newThread);
+
+      // Assert
+      expect(addedThread).toStrictEqual(new AddedThread({
+        id: 'thread-123',
+        title: 'thread title',
+        owner: 'user-123',
+      }));
+    });
+  });
+});
