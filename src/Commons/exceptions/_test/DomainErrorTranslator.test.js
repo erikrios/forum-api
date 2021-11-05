548089ebbd1d15b4d6fe6e07d8ddd664d464830c
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-05 20:21:59 +0700

    Refactor the code

--- src/Commons/exceptions/_test/DomainErrorTranslator.test.js
+++ src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -11,6 +11,10 @@ describe('DomainErrorTranslator', () => {
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'));
     expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'));
+    expect(DomainErrorTranslator.translate(new Error('NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY')))
+      .toStrictEqual(new InvariantError('tidak dapat membuat thread baru karena properti yang dibutuhkan tidak ada'));
+    expect(DomainErrorTranslator.translate(new Error('NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION')))
+      .toStrictEqual(new InvariantError('tidak dapat membuat thread baru karena tipe data tidak sesuai'));
   });
 
   it('should return original error when error message is not needed to translate', () => {
