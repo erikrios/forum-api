8bf66ea681200248e10ebf72415098ea71a8b7af
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-08 20:53:51 +0700

    Create CommentDetails.js

---
+++ src/Domains/comments/entities/CommentDetails.js
@@ -0,0 +1,28 @@
+class CommentDetails {
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    const {
+      id, username, date, content,
+    } = payload;
+
+    this.id = id;
+    this.username = username;
+    this.date = date;
+    this.content = content;
+  }
+
+  _verifyPayload({
+    id, username, date, content,
+  }) {
+    if (!id || !username || !date || !content) {
+      throw new Error('COMMENT_DETAILS.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof id !== 'string' || typeof username !== 'string' || typeof content !== 'string' || typeof date !== 'string') {
+      throw new Error('COMMENT_DETAILS.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = CommentDetails;
