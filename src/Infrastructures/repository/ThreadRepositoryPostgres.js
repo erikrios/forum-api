f5be2f8e94225ca9040e417597c3593234671909
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-08 12:25:59 +0700

    Add a function to check the thread existence

--- src/Infrastructures/repository/ThreadRepositoryPostgres.js
+++ src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -1,5 +1,6 @@
 const ThreadRepository = require('../../Domains/threads/ThreadRepository');
 const AddedThread = require('../../Domains/threads/entities/AddedThread');
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
 
 class ThreadRepositoryPostgres extends ThreadRepository {
   constructor(pool, idGenerator) {
@@ -20,6 +21,18 @@ class ThreadRepositoryPostgres extends ThreadRepository {
     const result = await this._pool.query(query);
 
     return new AddedThread({ ...result.rows[0] });
+  }
+
+  async isThreadExists(threadId) {
+    const query = {
+      text: 'SELECT COUNT(id) AS count FROM threads WHERE id = $1',
+      values: [threadId],
+    };
+
+    const result = await this._pool.query(query);
+    if (!Number(result.rows[0].count)) {
+      throw new NotFoundError(`Thread with id ${threadId} not found.`);
+    }
   }
 }
 
