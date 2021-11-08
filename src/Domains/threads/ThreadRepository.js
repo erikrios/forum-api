f29fc586b14e407686103afeaa035aae7419d15c
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-08 21:49:12 +0700

    Add a function to get a thread

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -6,6 +6,10 @@ class ThreadRepository {
   async isThreadExists(threadId) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async getThread(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ThreadRepository;
