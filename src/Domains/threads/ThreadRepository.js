d97ed0c4a264fd03e21168f148063db00e5312fd
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-08 11:11:12 +0700

    Add a function to check the thread existance

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -2,6 +2,10 @@ class ThreadRepository {
   async addThread(newThread) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async isThreadExists(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ThreadRepository;
