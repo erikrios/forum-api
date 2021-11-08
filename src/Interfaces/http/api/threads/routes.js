2fe09fce2970e5aae16938b8c11bc153ccb1920b
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-08 13:51:06 +0700

    Add post comment route

--- src/Interfaces/http/api/threads/routes.js
+++ src/Interfaces/http/api/threads/routes.js
@@ -7,6 +7,14 @@ const routes = (handler) => ([
       auth: 'forumapi_jwt',
     },
   },
+  {
+    method: 'POST',
+    path: '/threads/{threadId}/comments',
+    handler: handler.postThreadCommentHandler,
+    options: {
+      auth: 'forumapi_jwt',
+    },
+  },
 ]);
 
 module.exports = routes;
