89bcb5b8aa3bcb32796aff24d259ba35d72af64b
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-08 19:01:18 +0700

    Create deleteComment and verifyUserComment function

--- src/Infrastructures/repository/CommentRepositoryPostgres.js
+++ src/Infrastructures/repository/CommentRepositoryPostgres.js
@@ -1,5 +1,7 @@
 const CommentRepository = require('../../Domains/comments/CommentRepository');
 const AddedComment = require('../../Domains/comments/entities/AddedComment');
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
+const AuthorizationError = require('../../Commons/exceptions/AuthorizationError');
 
 class CommentRepositoryPostgres extends CommentRepository {
   constructor(pool, idGenerator) {
@@ -20,6 +22,31 @@ class CommentRepositoryPostgres extends CommentRepository {
     const result = await this._pool.query(query);
 
     return new AddedComment({ ...result.rows[0] });
+  }
+
+  async deleteComment(userId, threadId, commentId) {
+    const query = {
+      text: "UPDATE comments SET is_delete = 'true' WHERE id = $1 AND owner = $2 AND thread_id = $3",
+      values: [commentId, userId, threadId],
+    };
+
+    await this._pool.query(query);
+  }
+
+  async verifyUserComment(userId, commentId) {
+    const query = {
+      text: 'SELECT owner FROM comments WHERE id = $1',
+      values: [commentId],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError(`Comment with id ${commentId} not found.`);
+    }
+
+    if (result.rows[0].owner !== userId) {
+      throw new AuthorizationError('This resource is restricted to access.');
+    }
   }
 }
 
