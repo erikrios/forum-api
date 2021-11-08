f0ebe8f7bc100f46e6272043144e2360d8fcd601
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-08 13:50:58 +0700

    Add post comment handler

--- src/Interfaces/http/api/threads/handler.js
+++ src/Interfaces/http/api/threads/handler.js
@@ -1,10 +1,12 @@
 const AddThreadUseCase = require('../../../../Applications/use_case/AddThreadUseCase');
+const AddCommentUseCase = require('../../../../Applications/use_case/AddCommentUseCase');
 
 class ThreadsHandler {
   constructor(container) {
     this._container = container;
 
     this.postThreadHandler = this.postThreadHandler.bind(this);
+    this.postThreadCommentHandler = this.postThreadCommentHandler.bind(this);
   }
 
   async postThreadHandler(request, h) {
@@ -16,6 +18,22 @@ class ThreadsHandler {
       status: 'success',
       data: {
         addedThread,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async postThreadCommentHandler(request, h) {
+    const { id } = request.auth.credentials;
+    const { threadId } = request.params;
+    const addCommentUseCase = this._container.getInstance(AddCommentUseCase.name);
+    const addedComment = await addCommentUseCase.execute(id, threadId, request.payload);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedComment,
       },
     });
     response.code(201);
