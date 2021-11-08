bc5602e5ddc65792aad58581401e5af1025c1380
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-08 11:55:08 +0700

    Create AddCommentUseCase.test.js

---
+++ src/Applications/use_case/_test/AddCommentUseCase.test.js
@@ -0,0 +1,47 @@
+const NewComment = require('../../../Domains/comments/entities/NewComment');
+const AddedComment = require('../../../Domains/comments/entities/AddedComment');
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const AddCommentUseCase = require('../AddCommentUseCase');
+
+describe('AddCommentUseCase', () => {
+  it('should orchestrating the add comment action correctly', async () => {
+    // Arrange
+    const userId = 'user-123';
+    const threadId = 'thread-123';
+    const useCasePayload = {
+      content: 'new comment',
+    };
+
+    const expectedAddedComment = new AddedComment({
+      id: 'comment-123',
+      content: useCasePayload.content,
+      owner: userId,
+    });
+
+    /** creating dependency of use case */
+    const mockThreadRepository = new ThreadRepository();
+    const mockCommentRepository = new CommentRepository();
+
+    /** mocking needed function */
+    mockThreadRepository.isThreadExists = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.addComment = jest.fn()
+      .mockImplementation(() => Promise.resolve(expectedAddedComment));
+
+    /** creating use case instance */
+    const getCommentUseCase = new AddCommentUseCase({
+      commentRepository: mockCommentRepository,
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    const addedComment = await getCommentUseCase.execute(userId, threadId, useCasePayload);
+
+    // Assert
+    expect(addedComment).toStrictEqual(expectedAddedComment);
+    expect(mockThreadRepository.isThreadExists).toBeCalledWith(threadId);
+    expect(mockCommentRepository.addComment)
+      .toBeCalledWith(new NewComment(userId, threadId, useCasePayload));
+  });
+});
