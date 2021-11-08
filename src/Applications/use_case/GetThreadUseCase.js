a06fed9427377cc2f911a04d7d3ed91eda61ebbd
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-08 23:30:29 +0700

    Create GetThreadUseCase.js

---
+++ src/Applications/use_case/GetThreadUseCase.js
@@ -0,0 +1,20 @@
+class GetThreadUseCase {
+  constructor({ threadRepository }) {
+    this._threadRepository = threadRepository;
+  }
+
+  async execute(threadId) {
+    this._threadRepository.isThreadExists(threadId);
+    const threadDetails = await this._threadRepository.getThread(threadId);
+    threadDetails.comments.forEach((part, index, commentArrays) => {
+      if (part.isDelete) {
+        commentArrays[index].content = '**komentar telah dihapus**';
+      }
+      delete commentArrays[index].isDelete;
+    });
+
+    return threadDetails;
+  }
+}
+
+module.exports = GetThreadUseCase;
