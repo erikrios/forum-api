bcac3937f04e8dca20b5dda2a8f9bc15eb8ab54c
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-08 18:06:41 +0700

    Create deleteComment and verifyUserComment function

--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -2,6 +2,14 @@ class CommentRepository {
   async addComment(newComment) {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async deleteComment(userId, threadId, commentId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyUserComment(userId, commentId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = CommentRepository;
