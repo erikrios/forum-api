6e5cf9bcdcb486f6d167a9fb88c72d48134ff563
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2021-11-13 22:39:44 +0700

    Pass say hello world test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -49,6 +49,14 @@ const createServer = async (container) => {
     },
   ]);
 
+  await server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'Hello, World!',
+    }),
+  });
+
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
